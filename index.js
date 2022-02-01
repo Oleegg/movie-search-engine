db41c5a9a4f639c139f3ee472f5048feb08f6993
Oleegg <91334112+Oleegg@users.noreply.github.com>
2022-02-01 11:39:42 +0300

    add cards for posters

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let url = `https://api.themoviedb.org/3/search/movie?query=spring&api_key=58f80e50b7e31f11334091c645181f83`
+let url = `https://api.themoviedb.org/3/search/movie?query=girls&api_key=58f80e50b7e31f11334091c645181f83`
 
 const contentGallery = document.querySelector('.content')
 
@@ -12,17 +12,30 @@ getData();
 
 
 
-//   results", затем индекс элемента в массиве 0, затем ключ "poster_path
-//ки "https://image.tmdb.org/t/p/w1280"
+//overview - описание...release_date...
 
 
 function showData(d) {
     for (let i = 0; i < d.results.length; i++) {
+        const title = document.createElement('h2')
+        title.classList.add('title-img')
+        const average = document.createElement('p')
+        title.classList.add('title-img')
+        const div = document.createElement('div')
+        div.classList.add('wrapper-img')
+        const divAbout = document.createElement('div')
+        divAbout.classList.add('about-img')
         const img = document.createElement('img');
         img.classList.add('gallery-img')
         img.src = `https://image.tmdb.org/t/p/w1280${d.results[i].poster_path}`;
         img.alt = `image`;
-        contentGallery.append(img);
+        contentGallery.append(div);
+        div.append(img)
+        div.append(divAbout)
+        divAbout.append(title)
+        divAbout.append(average)
+        title.innerHTML = `${d.results[i].original_title}`
+        average.innerHTML = `${d.results[i].vote_average}`
     }
 }
 
